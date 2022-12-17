04e07f57ec767cad867c927669c261c8f9ca317b
HenriqueMiossi <henriquemiossi@gmail.com>
2022-12-17 19:21:35 -0300

    Add project files.

---
+++ PagesDemo/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace PagesDemo;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		var navigationPage = new NavigationPage(new MainPage());
+		navigationPage.BarTextColor = Colors.Yellow;
+
+		MainPage = navigationPage;
+	}
+}
